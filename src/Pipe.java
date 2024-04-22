93d969b2aba56b0c5d0e0481c7b040ca25e48741
ridwanosman04 <150202272+ridwanosman04@users.noreply.github.com>
2024-04-22 17:06:32 -0500

    so far only bird
    dysg

--- src/Pipe.java
+++ src/Pipe.java
@@ -1,5 +1,11 @@
-public class Pipe {
+import edu.macalester.graphics.Image;
+
+public class Pipe  extends Image{
     // the pipes can't touch each other, generate at different heights, induce the game to stop if the bird touches
     // the pipes
+    private int x;
+    private int yTop;
+    private int yBottomon;
+    private Image b
 
 }
