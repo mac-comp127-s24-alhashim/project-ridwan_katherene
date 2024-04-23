ab7e48f121dbeb67f2879f8e37fa4de3d4acc0f2
ridwanosman04 <150202272+ridwanosman04@users.noreply.github.com>
2024-04-22 19:04:53 -0500

    another
    k

--- src/flappyBird.java
+++ src/flappyBird.java
@@ -2,6 +2,20 @@ import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.Image;
 import java.awt.Dimension;
 
-public class FlappyBird extends {
+public class FlappyBird extends CanvasWindow {
+
+    private Bird bird;
+    private static final int WINDOW_WIDTH = 800;
+    private static final int WINDOW_HEIGHT = 600;
+
+    public FlappyBird() {
+        super("Flappy Bird", WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        bird = new Bird(100,200,50, 50, new Image ("flappyBird.png"));
+            add(bird);
+    }
+    public static void main(String[] args) {
+        new GameWindow();
+    }
     
 }
--- src/scoreboard.java
+++ src/scoreboard.java
@@ -1,3 +1,19 @@
 public class scoreboard {
-    
+    private int score;
+
+    public Scoreboard(){
+        score = 0;
+    }
+    public void increment(){
+        score++;
+    }
+    public int getScore(){
+        return score;
+    }
+    public void resetScore(){
+        score = 0;
+    }
+    public void draw(Graphics g){
+        g.drawString("Score:"+ score, 10, 20);
+    }
 }
